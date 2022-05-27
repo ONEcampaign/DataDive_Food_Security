d907664af36e7e3e0fc0af57719fea56be3f61e5
lpicci96 <lpicci96@gmail.com>
2022-05-27 15:04:04 -0600

    updating charts

--- scripts/charts.py
+++ scripts/charts.py
@@ -1,10 +1,10 @@
 """ """
 
 import pandas as pd
-import numpy as np
 from scripts import utils, config
 from scripts.analysis import get_stunting_wb, get_fao_undernourishment, get_usda_food_exp, get_ipc, get_food_price_index
-import country_converter as coco
+from scripts.commodity_analysis import get_commodity_prices, get_indices
+from typing import Optional
 
 
 def fao_fpi_main(start_date:str = '2000-01-01') -> None:
@@ -14,50 +14,43 @@ def fao_fpi_main(start_date:str = '2000-01-01') -> None:
     df.assign(date_popup = lambda d: d.date).loc[df.date>=start_date].to_csv(f'{config.paths.output}/fao_fpi_main.csv', index=False)
 
 
+def undernourishment_world() -> None:
+    """
+    Create undernourishment chart for world, from FAO food security data
+    (Chart not used in page)
+    """
 
-
-
-# ================================================================================
-# undernourishment
-# ================================================================================
-
-def _undernourishment_world(df:pd.DataFrame) -> None:
-    """Create undernourishment chart from FAO food security data"""
-
+    df = get_fao_undernourishment()
 
     pct_df = df.loc[df.item == 'Prevalence of undernourishment (percent) (annual value)', ['area', 'year', 'value', 'value_text']]
     mil_df = df.loc[df.item == 'Number of people undernourished (million) (annual value)', ['area', 'year', 'value', 'value_text']]
-
     final =  pd.merge(pct_df, mil_df, on=['area', 'year'], how='inner', suffixes=('_pct', '_mil'))
     final[final.area == 'World'].to_csv(f'{config.paths.output}/undernourishment_world.csv', index=False)
 
-def undernourishment() -> None:
-    """ """
 
-    fao_df = get_fao_undernourishment()
+def stunting_map() -> None:
+    """
+    creates stunting map - by country for latest available data point
+    (not used in main page)
+    """
 
-    _undernourishment_world(fao_df)
-
-# ================================================================================
-# stunting
-# ================================================================================
-
-def _stunting_map(df:pd.DataFrame) -> None:
-    """create stunting map - by country for latest available data point"""
+    df = get_stunting_wb()
 
     (utils.get_latest_values(df, 'iso_code', 'year')
      .pipe(utils.add_flourish_geometries)
      .to_csv(f'{config.paths.output}/stunting_map.csv', index=False))
 
-def _stunting_top_countries_bar(df:pd.DataFrame) -> None:
-    """ """
+
+def stunting_top_countries_bar() -> None:
+    """Creates a chart for 30 countries with highest stunting values + SSA"""
+
+    df = get_stunting_wb()
 
     ssf = (df.loc[df.iso_code == 'SSF']
            .pipe(utils.get_latest_values, 'iso_code', 'year')) #get latest value for Sub-Saharan Africa
 
     df = (utils
      .get_latest_values(df, 'iso_code', 'year')
-     #.pipe(utils.filter_countries, 'continent', ['Africa'])
      .sort_values(by='value', ascending=False)
      .pipe(utils.keep_countries)
      .reset_index(drop=True)
@@ -67,51 +60,8 @@ def _stunting_top_countries_bar(df:pd.DataFrame) -> None:
     df.to_csv(f'{config.paths.output}/stunting_top_countries_bar.csv', index=False)
 
 
-def _stunting_vs_gdppc(df:pd.DataFrame) -> None:
-    """ """
-
-    df =   (utils.keep_countries(df)
-            .pipe(utils.get_latest_values, 'iso_code', 'year')
-            .pipe(utils.add_gdp_latest, per_capita=True)
-            .dropna(subset='gdp_per_capita').assign(continent = lambda d: coco.convert(d.iso_code, to='continent')))
-    df.loc[df.continent != 'Africa', 'continent'] = np.nan
-    df.to_csv(f'{config.paths.output}/stunting_vs_gdppc.csv', index=False)
-
-
-def stunting_charts() -> None:
-    """Update stunting charts"""
-
-    stunting = get_stunting_wb() # get stunting data
-
-    _stunting_map(stunting) #update stunting map
-    _stunting_top_countries_bar(stunting)
-    _stunting_vs_gdppc(stunting)
-
-
-# ========================================================
-# food expenditure share
-# ========================================================
-
-def food_exp_share_chart() -> None:
-    """ """
-
-    df = get_usda_food_exp()
-    df = (utils.add_gdp_latest(df, iso_col='iso_code', per_capita = True).pipe(utils.add_income_levels))
-
-    df.loc[df.income_level.isin(['Low income', 'Lower middle income']), 'income_level'] = 'Low/lower middle income'
-    df.loc[df.income_level.isin(['High income', 'Upper middle income']), 'income_level'] = 'High/higher middle income'
-
-    df.to_csv(f'{config.paths.output}/food_share_chart.csv', index=False)
-
-
-
-# ===========================================================
-# IPC charts
-# ===============================================================
-
-
 def ipc_charts() -> None:
-    """ """
+    """Create charts for all IPC phases"""
 
     phases = {'Phase 2':'phase_2', 'Phase 3':'phase_3',
               'Phase 4':'phase_4', 'Phase 5':'phase_5', 'Phase 3+':'phase_3plus'}
@@ -127,9 +77,49 @@ def ipc_charts() -> None:
         .to_csv(f'{config.paths.output}/ipc_{phase}.csv', index=False))
 
 
+def food_exp_share_chart() -> None:
+    """Creates scatter plot of share of food expenditure vs gdp per capita"""
+
+    df = get_usda_food_exp()
+    df = (utils.add_gdp_latest(df, iso_col='iso_code', per_capita = True).pipe(utils.add_income_levels).assign(income_level_agg = lambda d: d.income_level))
+
+    df.loc[df.income_level_agg.isin(['Low income', 'Lower middle income']), 'income_level_agg'] = 'Low/lower middle income'
+    df.loc[df.income_level_agg.isin(['High income', 'Upper middle income']), 'income_level_agg'] = 'High/higher middle income'
+
+    df.to_csv(f'{config.paths.output}/food_share_chart.csv', index=False)
 
 
 
+def fao_fpi_scrolly(start_date:str = '2010-01-01') -> None:
+    """Creates csv for FAO Food Price Index Chart starting in 2014-01-01 to embed in the scolly story"""
+
+    df = get_food_price_index()
+    df.assign(date_popup = lambda d: d.date).loc[df.date>=start_date].to_csv(f'{config.paths.output}/fao_fpi_scrolly.csv', index=False)
 
 
+def commodity_chart(commodities: Optional[list] = ['Palm oil', 'Sunflower oil', 'Maize', 'Wheat']) -> None:
+    """Creates chart for WB commodity prices"""
+
+    df = get_commodity_prices(commodities)
+    (df.assign(date_popup = lambda d: d.period)
+     .loc[df.period>='2010-01-01']
+     .to_csv(f'{config.paths.output}/food_commodity_chart.csv', index=False))
+
+def index_chart(indexes:Optional[list] = ['Agriculture', 'Food', 'Oils & Meals', 'Grains', 'Other Food', 'Fertilizers']) -> None:
+    """
+    Creates chart for WB index
+    (Not Used in main page)
+    """
+
+    df = get_indices(indexes)
+    df.loc[df.period>='2010-01-01'].to_csv(f'{config.paths.output}/index_chart.csv', index=False)
+
+
+def restriction_chart() -> None:
+    """
+    extract data from IFPRI article to recreate chart
+    https://www.ifpri.org/blog/bad-worse-how-export-restrictions-exacerbate-global-food-security
+    """
+
+    pass
 
