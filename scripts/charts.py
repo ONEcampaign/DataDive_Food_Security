f936966c5ccc393c5f679c780a9359fd3ea4cf60
Black formatter <data@one.org>
2022-06-02 15:01:53 +0000

    Fix code style issues with Black

--- scripts/charts.py
+++ scripts/charts.py
@@ -2,16 +2,27 @@
 
 import pandas as pd
 from scripts import utils, config
-from scripts.analysis import get_stunting_wb, get_fao_undernourishment, get_usda_food_exp, get_ipc, get_food_price_index, get_commodity_prices, get_indices, get_fao_fertilizer
+from scripts.analysis import (
+    get_stunting_wb,
+    get_fao_undernourishment,
+    get_usda_food_exp,
+    get_ipc,
+    get_food_price_index,
+    get_commodity_prices,
+    get_indices,
+    get_fao_fertilizer,
+)
 from typing import Optional
 import country_converter as coco
 
 
-def fao_fpi_main(start_date:str = '2000-01-01') -> None:
+def fao_fpi_main(start_date: str = "2000-01-01") -> None:
     """Creates csv for FAO Food Price Index Chart starting in 2000-01-01"""
 
     df = get_food_price_index()
-    df.assign(date_popup = lambda d: d.date).loc[df.date>=start_date].to_csv(f'{config.paths.output}/fao_fpi_main.csv', index=False)
+    df.assign(date_popup=lambda d: d.date).loc[df.date >= start_date].to_csv(
+        f"{config.paths.output}/fao_fpi_main.csv", index=False
+    )
 
 
 def undernourishment_world() -> None:
@@ -22,10 +33,20 @@ def undernourishment_world() -> None:
 
     df = get_fao_undernourishment()
 
-    pct_df = df.loc[df.item == 'Prevalence of undernourishment (percent) (annual value)', ['area', 'year', 'value', 'value_text']]
-    mil_df = df.loc[df.item == 'Number of people undernourished (million) (annual value)', ['area', 'year', 'value', 'value_text']]
-    final =  pd.merge(pct_df, mil_df, on=['area', 'year'], how='inner', suffixes=('_pct', '_mil'))
-    final[final.area == 'World'].to_csv(f'{config.paths.output}/undernourishment_world.csv', index=False)
+    pct_df = df.loc[
+        df.item == "Prevalence of undernourishment (percent) (annual value)",
+        ["area", "year", "value", "value_text"],
+    ]
+    mil_df = df.loc[
+        df.item == "Number of people undernourished (million) (annual value)",
+        ["area", "year", "value", "value_text"],
+    ]
+    final = pd.merge(
+        pct_df, mil_df, on=["area", "year"], how="inner", suffixes=("_pct", "_mil")
+    )
+    final[final.area == "World"].to_csv(
+        f"{config.paths.output}/undernourishment_world.csv", index=False
+    )
 
 
 def stunting_map() -> None:
@@ -36,9 +57,11 @@ def stunting_map() -> None:
 
     df = get_stunting_wb()
 
-    (utils.get_latest_values(df, 'iso_code', 'year')
-     .pipe(utils.add_flourish_geometries)
-     .to_csv(f'{config.paths.output}/stunting_map.csv', index=False))
+    (
+        utils.get_latest_values(df, "iso_code", "year")
+        .pipe(utils.add_flourish_geometries)
+        .to_csv(f"{config.paths.output}/stunting_map.csv", index=False)
+    )
 
 
 def stunting_top_countries_bar() -> None:
@@ -46,82 +69,119 @@ def stunting_top_countries_bar() -> None:
 
     df = get_stunting_wb()
 
-    ssf = (df.loc[df.iso_code == 'SSF']
-           .pipe(utils.get_latest_values, 'iso_code', 'year')) #get latest value for Sub-Saharan Africa
+    ssf = df.loc[df.iso_code == "SSF"].pipe(
+        utils.get_latest_values, "iso_code", "year"
+    )  # get latest value for Sub-Saharan Africa
 
-    df = (utils
-     .get_latest_values(df, 'iso_code', 'year')
-     .sort_values(by='value', ascending=False)
-     .pipe(utils.keep_countries)
-     .reset_index(drop=True)
-     .loc[0:30])
+    df = (
+        utils.get_latest_values(df, "iso_code", "year")
+        .sort_values(by="value", ascending=False)
+        .pipe(utils.keep_countries)
+        .reset_index(drop=True)
+        .loc[0:30]
+    )
 
     df = df.append(ssf)
-    df.to_csv(f'{config.paths.output}/stunting_top_countries_bar.csv', index=False)
+    df.to_csv(f"{config.paths.output}/stunting_top_countries_bar.csv", index=False)
 
 
 def ipc_charts() -> None:
     """Create charts for all IPC phases"""
 
-    phases = {'Phase 2':'phase_2', 'Phase 3':'phase_3',
-              'Phase 4':'phase_4', 'Phase 5':'phase_5', 'Phase 3+':'phase_3plus'}
+    phases = {
+        "Phase 2": "phase_2",
+        "Phase 3": "phase_3",
+        "Phase 4": "phase_4",
+        "Phase 5": "phase_5",
+        "Phase 3+": "phase_3plus",
+    }
     df = get_ipc()
 
     for phase in phases.values():
         df_phase = df.copy(deep=True)
-        (df_phase
-        .sort_values(by = phase, ascending=False)
-        .reset_index(drop=True)
-        .loc[0:15, ['country', phase, 'period_start', 'period_end', 'source']]
-        .dropna(subset = phase)
-        .to_csv(f'{config.paths.output}/ipc_{phase}.csv', index=False))
+        (
+            df_phase.sort_values(by=phase, ascending=False)
+            .reset_index(drop=True)
+            .loc[0:15, ["country", phase, "period_start", "period_end", "source"]]
+            .dropna(subset=phase)
+            .to_csv(f"{config.paths.output}/ipc_{phase}.csv", index=False)
+        )
 
 
 def food_exp_share_chart() -> None:
     """Creates scatter plot of share of food expenditure vs gdp per capita"""
 
     df = get_usda_food_exp()
-    df = (utils.add_gdp_latest(df, iso_col='iso_code', per_capita = True).pipe(utils.add_income_levels).assign(income_level_agg = lambda d: d.income_level))
+    df = (
+        utils.add_gdp_latest(df, iso_col="iso_code", per_capita=True)
+        .pipe(utils.add_income_levels)
+        .assign(income_level_agg=lambda d: d.income_level)
+    )
 
-    df.loc[df.income_level_agg.isin(['Low income', 'Lower middle income']), 'income_level_agg'] = 'Low/lower middle income'
-    df.loc[df.income_level_agg.isin(['High income', 'Upper middle income']), 'income_level_agg'] = 'High/higher middle income'
+    df.loc[
+        df.income_level_agg.isin(["Low income", "Lower middle income"]),
+        "income_level_agg",
+    ] = "Low/lower middle income"
+    df.loc[
+        df.income_level_agg.isin(["High income", "Upper middle income"]),
+        "income_level_agg",
+    ] = "High/higher middle income"
 
-    df.to_csv(f'{config.paths.output}/food_share_chart.csv', index=False)
+    df.to_csv(f"{config.paths.output}/food_share_chart.csv", index=False)
 
 
-
-def fao_fpi_scrolly(start_date:str = '2010-01-01') -> None:
+def fao_fpi_scrolly(start_date: str = "2010-01-01") -> None:
     """Creates csv for FAO Food Price Index Chart starting in 2014-01-01 to embed in the scolly story"""
 
     df = get_food_price_index()
-    df.assign(date_popup = lambda d: d.date).loc[df.date>=start_date].to_csv(f'{config.paths.output}/fao_fpi_scrolly.csv', index=False)
+    df.assign(date_popup=lambda d: d.date).loc[df.date >= start_date].to_csv(
+        f"{config.paths.output}/fao_fpi_scrolly.csv", index=False
+    )
 
 
-def commodity_chart(commodities: Optional[list] = ['Palm oil', 'Sunflower oil', 'Maize', 'Wheat']) -> None:
+def commodity_chart(
+    commodities: Optional[list] = ["Palm oil", "Sunflower oil", "Maize", "Wheat"]
+) -> None:
     """Creates chart for WB commodity prices"""
 
     df = get_commodity_prices(commodities)
-    (df.assign(date_popup = lambda d: d.period)
-     .loc[df.period>='2010-01-01']
-     .to_csv(f'{config.paths.output}/food_commodity_chart.csv', index=False))
+    (
+        df.assign(date_popup=lambda d: d.period)
+        .loc[df.period >= "2010-01-01"]
+        .to_csv(f"{config.paths.output}/food_commodity_chart.csv", index=False)
+    )
 
-def index_chart(indexes:Optional[list] = ['Agriculture', 'Food', 'Oils & Meals', 'Grains', 'Other Food', 'Fertilizers']) -> None:
+
+def index_chart(
+    indexes: Optional[list] = [
+        "Agriculture",
+        "Food",
+        "Oils & Meals",
+        "Grains",
+        "Other Food",
+        "Fertilizers",
+    ]
+) -> None:
     """
     Creates chart for WB index
     (Not Used in main page)
     """
 
     df = get_indices(indexes)
-    df.loc[df.period>='2010-01-01'].to_csv(f'{config.paths.output}/index_chart.csv', index=False)
+    df.loc[df.period >= "2010-01-01"].to_csv(
+        f"{config.paths.output}/index_chart.csv", index=False
+    )
 
 
 def ifpri_restriction_chart() -> None:
     """Create trade restriction chart from IFPRI"""
 
-    df = pd.read_csv(f'{config.paths.raw_data}/restrictions_data.csv')
-    (df.rename(columns={"Ukraine Crisis [2022]": "Russia's war in Ukraine [2022]" })
-     .to_csv(f'{config.paths.output}/ifpri_restriction.csv', index=False))
-
+    df = pd.read_csv(f"{config.paths.raw_data}/restrictions_data.csv")
+    (
+        df.rename(
+            columns={"Ukraine Crisis [2022]": "Russia's war in Ukraine [2022]"}
+        ).to_csv(f"{config.paths.output}/ifpri_restriction.csv", index=False)
+    )
 
 
 def potash_dependence_chart() -> None:
@@ -129,10 +189,12 @@ def potash_dependence_chart() -> None:
 
     df = get_fao_fertilizer()
 
-    (df.pipe(utils.add_flourish_geometries)
-     .loc[:, ['flourish_geom', 'iso_code', 'country', 'dependence']]
-    .assign(country = lambda d: coco.convert(d.iso_code, to='name_short'))
-    .to_csv(f'{config.paths.output}/potash_map.csv', index=False))
+    (
+        df.pipe(utils.add_flourish_geometries)
+        .loc[:, ["flourish_geom", "iso_code", "country", "dependence"]]
+        .assign(country=lambda d: coco.convert(d.iso_code, to="name_short"))
+        .to_csv(f"{config.paths.output}/potash_map.csv", index=False)
+    )
 
 
 def update_charts() -> None:
@@ -148,7 +210,6 @@ def update_charts() -> None:
     potash_dependence_chart()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     update_charts()
-    print('Successfully updated charts')
-
+    print("Successfully updated charts")
--- scripts/utils.py
+++ scripts/utils.py
@@ -6,7 +6,10 @@ import pandas as pd
 import weo
 import country_converter as coco
 
-def add_flourish_geometries(df: pd.DataFrame, key_column_name: str = 'iso_code') -> pd.DataFrame:
+
+def add_flourish_geometries(
+    df: pd.DataFrame, key_column_name: str = "iso_code"
+) -> pd.DataFrame:
     """
     Adds a geometry column to a dataframe based on iso3 code
         df: DataFrame to add a column
@@ -16,18 +19,19 @@ def add_flourish_geometries(df: pd.DataFrame, key_column_name: str = 'iso_code')
     g = pd.read_json(f"{config.paths.glossaries}/flourish_geometries_world.json")
     g = (
         g.rename(columns={g.columns[0]: "flourish_geom", g.columns[1]: key_column_name})
-            .iloc[1:]
-            .drop_duplicates(subset=key_column_name, keep="first")
-            .reset_index(drop=True)
+        .iloc[1:]
+        .drop_duplicates(subset=key_column_name, keep="first")
+        .reset_index(drop=True)
     )
 
     return pd.merge(g, df, on=key_column_name, how="left")
 
 
-def remove_unnamed_cols(df:pd.DataFrame) -> pd.DataFrame:
-    """removes all columns with 'Unnamed' """
+def remove_unnamed_cols(df: pd.DataFrame) -> pd.DataFrame:
+    """removes all columns with 'Unnamed'"""
 
-    return df.loc[:, ~df.columns.str.contains('Unnamed')]
+    return df.loc[:, ~df.columns.str.contains("Unnamed")]
+
 
 def clean_numeric_column(column: pd.Series) -> pd.Series:
     """removes commas and transforms pandas series to numeric"""
@@ -37,20 +41,27 @@ def clean_numeric_column(column: pd.Series) -> pd.Series:
 
     return column
 
-def get_latest_values(df:pd.DataFrame, grouping_col:str, date_col:str) -> pd.DataFrame:
+
+def get_latest_values(
+    df: pd.DataFrame, grouping_col: str, date_col: str
+) -> pd.DataFrame:
     """returns a dataframe with only latest values per group"""
 
-    return (df.loc[df.groupby(grouping_col)[date_col].transform(max) == df[date_col]]
-            .reset_index(drop=True))
+    return df.loc[
+        df.groupby(grouping_col)[date_col].transform(max) == df[date_col]
+    ].reset_index(drop=True)
 
 
-def keep_countries(df:pd.DataFrame, iso_col:str = 'iso_code') -> pd.DataFrame:
+def keep_countries(df: pd.DataFrame, iso_col: str = "iso_code") -> pd.DataFrame:
     """returns a dataframe with only countries"""
 
     cc = coco.CountryConverter()
-    return df[df[iso_col].isin(cc.data['ISO3'])].reset_index(drop=True)
+    return df[df[iso_col].isin(cc.data["ISO3"])].reset_index(drop=True)
 
-def filter_countries(df:pd.DataFrame, by: str, values:list = ['Africa'], iso_col:str = 'iso_code') -> pd.DataFrame:
+
+def filter_countries(
+    df: pd.DataFrame, by: str, values: list = ["Africa"], iso_col: str = "iso_code"
+) -> pd.DataFrame:
     """
     returns a filtered dataframe
         by: filtering category -'continent', UNregion etc.
@@ -59,17 +70,16 @@ def filter_countries(df:pd.DataFrame, by: str, values:list = ['Africa'], iso_col
 
     cc = coco.CountryConverter()
     if by not in cc.data.columns:
-        raise ValueError(f'{by} is not valid')
+        raise ValueError(f"{by} is not valid")
 
     df[by] = coco.convert(df[iso_col], to=by)
-    return (df[df[by]
-            .isin(values)]
-            .drop(columns = by)
-            .reset_index(drop=True))
+    return df[df[by].isin(values)].drop(columns=by).reset_index(drop=True)
+
 
 # ============================================================================
 # Income levels
 # ============================================================================
+
 
 def get_income_levels() -> pd.DataFrame:
     """Downloads fresh version of income levels from WB"""
@@ -86,16 +96,20 @@ def get_income_levels() -> pd.DataFrame:
 
     return df
 
-def add_income_levels(df:pd.DataFrame, iso_col:str = 'iso_code') -> pd.DataFrame:
+
+def add_income_levels(df: pd.DataFrame, iso_col: str = "iso_code") -> pd.DataFrame:
     """Add income levels to a dataframe"""
 
-    income_levels = get_income_levels().set_index("Code").loc[:, "Income group"].to_dict()
-    return df.assign(income_level = lambda d: d[iso_col].map(income_levels))
+    income_levels = (
+        get_income_levels().set_index("Code").loc[:, "Income group"].to_dict()
+    )
+    return df.assign(income_level=lambda d: d[iso_col].map(income_levels))
 
 
 # ===================================================
 # World Bank API
 # ===================================================
+
 
 def _download_wb_data(code: str, database: int = 2) -> pd.DataFrame:
     """
@@ -112,6 +126,7 @@ def _download_wb_data(code: str, database: int = 2) -> pd.DataFrame:
     except:
         raise Exception(f"Could not retieve {code} indicator from World Bank")
 
+
 def _melt_wb_data(df: pd.DataFrame) -> pd.DataFrame:
     """Melts dataframe extracted from World Bank from wide to "long" format"""
 
@@ -121,6 +136,7 @@ def _melt_wb_data(df: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
+
 def get_wb_indicator(code: str, database: int = 2) -> pd.DataFrame:
     """
     Steps to extract and clean an indicator from World Bank
@@ -128,7 +144,7 @@ def get_wb_indicator(code: str, database: int = 2) -> pd.DataFrame:
         database: database number, default = 2 (World Development Indicators)
     """
 
-    df = (_download_wb_data(code, database).pipe(_melt_wb_data))
+    df = _download_wb_data(code, database).pipe(_melt_wb_data)
     print(f"Successfully extracted {code} from World Bank")
 
     return df
@@ -140,6 +156,7 @@ def get_wb_indicator(code: str, database: int = 2) -> pd.DataFrame:
 
 WEO_YEAR = 2022
 WEO_RELEASE = 1
+
 
 def _download_weo(year: int = WEO_YEAR, release: int = WEO_RELEASE) -> None:
     """Downloads WEO as a csv to raw data folder as "weo_month_year.csv"""
@@ -153,6 +170,7 @@ def _download_weo(year: int = WEO_YEAR, release: int = WEO_RELEASE) -> None:
         )
     except ConnectionError:
         raise ConnectionError("Could not download weo data")
+
 
 def _clean_weo(df: pd.DataFrame) -> pd.DataFrame:
     """cleans and formats weo dataframe"""
@@ -173,52 +191,66 @@ def _clean_weo(df: pd.DataFrame) -> pd.DataFrame:
     ]
     return (
         df.drop(cols_to_drop, axis=1)
-            .rename(columns=columns)
-            .melt(id_vars=columns.values(), var_name="year", value_name="value")
-            .assign(
+        .rename(columns=columns)
+        .melt(id_vars=columns.values(), var_name="year", value_name="value")
+        .assign(
             value=lambda d: d.value.map(
                 lambda x: str(x).replace(",", "").replace("-", "")
             )
         )
-            .astype({"year": "int32"})
-            .assign(value=lambda d: pd.to_numeric(d.value, errors="coerce"))
+        .astype({"year": "int32"})
+        .assign(value=lambda d: pd.to_numeric(d.value, errors="coerce"))
     )
 
-def get_weo_indicator_latest(indicator:str, target_year: int = 2022, *, min_year: int = 2018) -> pd.DataFrame:
+
+def get_weo_indicator_latest(
+    indicator: str, target_year: int = 2022, *, min_year: int = 2018
+) -> pd.DataFrame:
     """
     Retrieves values for an indicator for a target year
     """
 
     df = weo.WEO(f"{config.paths.raw_data}/weo_{WEO_YEAR}_{WEO_RELEASE}.csv").df
 
-    df = (df
-          .pipe(_clean_weo)
-          .dropna(subset = ['value'])
-          .loc[lambda d: (d.indicator == indicator)
-                         &(d.year>=min_year)
-                         &(d.year<=target_year),["iso_code", "year","value"]]
-          .reset_index(drop=True))
-    return df.loc[df.groupby(['iso_code'])['year'].transform(max) == df['year'], ['iso_code', 'value']]
+    df = (
+        df.pipe(_clean_weo)
+        .dropna(subset=["value"])
+        .loc[
+            lambda d: (d.indicator == indicator)
+            & (d.year >= min_year)
+            & (d.year <= target_year),
+            ["iso_code", "year", "value"],
+        ]
+        .reset_index(drop=True)
+    )
+    return df.loc[
+        df.groupby(["iso_code"])["year"].transform(max) == df["year"],
+        ["iso_code", "value"],
+    ]
 
 
-def get_gdp_latest(per_capita:bool = False, year:int = 2022) -> pd.DataFrame:
+def get_gdp_latest(per_capita: bool = False, year: int = 2022) -> pd.DataFrame:
     """
     return latest gdp values
         set per_capita = True to return gdp per capita values
     """
     if per_capita:
-        return get_weo_indicator_latest(target_year = year, indicator='NGDPDPC')
+        return get_weo_indicator_latest(target_year=year, indicator="NGDPDPC")
     else:
-        return get_weo_indicator_latest(target_year = year, indicator='NGDPD').assign(value = lambda d: d.value*1e9)
+        return get_weo_indicator_latest(target_year=year, indicator="NGDPD").assign(
+            value=lambda d: d.value * 1e9
+        )
 
 
-def add_gdp_latest(df:pd.DataFrame, iso_col:str = 'iso_code', per_capita = False, year: int = 2022) -> pd.DataFrame:
+def add_gdp_latest(
+    df: pd.DataFrame, iso_col: str = "iso_code", per_capita=False, year: int = 2022
+) -> pd.DataFrame:
     """adds a column with latest gdp values to a dataframe"""
 
     if per_capita:
-        new_col_name = 'gdp_per_capita'
+        new_col_name = "gdp_per_capita"
     else:
-        new_col_name = 'gdp'
+        new_col_name = "gdp"
 
     gdp_df = get_gdp_latest(year=year, per_capita=per_capita)
     gdp_dict = gdp_df.set_index("iso_code")["value"].to_dict()
@@ -226,4 +258,3 @@ def add_gdp_latest(df:pd.DataFrame, iso_col:str = 'iso_code', per_capita = False
     df[new_col_name] = df[iso_col].map(gdp_dict)
 
     return df
-
