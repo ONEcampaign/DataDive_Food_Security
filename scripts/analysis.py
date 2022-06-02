4226ce08592ef4ea0467b6e8edb3bdbb5e8c997b
lpicci96 <lpicci96@gmail.com>
2022-06-02 07:42:49 -0600

    removing DHS

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -9,10 +9,6 @@ from typing import Optional
 import requests
 
 
-# ======================================================================================================================
-# Extraction tools
-# ======================================================================================================================
-
 def get_stunting_wb() -> pd.DataFrame:
     """
     Extract indicator Prevalence of stunting (SH.STA.STNT.ME.ZS) from World Bank
@@ -21,38 +17,6 @@ def get_stunting_wb() -> pd.DataFrame:
     return (utils.get_wb_indicator(code = 'SH.STA.STNT.ME.ZS', database=2)
             .dropna(subset='value')
             .reset_index(drop=True))
-
-
-# DHS extraction tools
-def __clean_dhs(df:pd.DataFrame) -> pd.DataFrame:
-    """Cleans raw data from DHS"""
-
-    columns = {'Country Name':'country',
-               'Survey Year':'year',
-               'Survey Name':'survey_name',
-               'Indicator':'indicator',
-               'Characteristic Category': 'category_name',
-               'Characteristic Label': 'category_label',
-               'Value':'value'}
-
-    df = (df
-          .rename(columns = columns)
-          .dropna(subset = 'indicator')
-          .filter(columns.values(), axis=1)
-          .assign(iso_code = lambda d: coco.convert(d.country))
-          .reset_index(drop=True)
-          )
-
-    return df
-
-def get_dhs_indicator(indicator:str, category_name:str) -> pd.DataFrame:
-    """ """
-
-    df = pd.read_excel(f'{config.paths.raw_data}/DHS_data.xlsx')
-    df = __clean_dhs(df)
-
-    return df.loc[(df['indicator'] == indicator)&(df['category_name'] == category_name)]
-
 
 #FAO Food index
 def __read_fao_food_price_index(parser:Optional[str] = 'html.parser',
